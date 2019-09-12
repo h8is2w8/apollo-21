5a214fb81582230399e5ee75fed3bfe104e025c0
h8is2w8 <toscatland@gmail.com>
2019-09-12 11:20:20 +0300

    Remove redundant constants

--- src/index.js
+++ src/index.js
@@ -12,18 +12,20 @@ const CANVAS_HEIGHT = 800;
 const WORLD_WIDTH = CANVAS_WIDTH / 2;
 const WORLD_HEIGHT = CANVAS_HEIGHT / 2;
 
-const MOON  = new CelestialObject({ r: 5,  orbitR: 1, color: 'grey'  });
-const VENUS = new CelestialObject({ r: 8,  orbitR: 3, color: 'red'   });
-const MARS  = new CelestialObject({ r: 12, orbitR: 5, color: 'brown' });
-const EARTH = new CelestialObject({
-  r: 10, orbitR: 10, color: 'blue',
-  drawOrbits: true, satellites: [MOON]
-});
-const SUN   = new CelestialObject({
+const SUN = new CelestialObject({
   pos: new Vec(WORLD_WIDTH / 2, WORLD_HEIGHT / 2),
-  r: 25, orbitR: 40, color: 'orange',
+  r: 25, orbitR: 40, c: 'orange',
   drawOrbits: true,
-  satellites: [VENUS, EARTH, MARS, null]
+  satellites: [
+    new CelestialObject({ r: 8, orbitR: 3, c: 'red' }),
+    new CelestialObject({
+      r: 10, orbitR: 10, c: 'blue', drawOrbits: true,
+      satellites: [
+        new CelestialObject({ r: 5,  orbitR: 1, c: 'grey' })
+      ]
+    }),
+    new CelestialObject({ r: 12, orbitR: 5, c: 'brown' }),
+  ]
 });
 
 // Preset World
